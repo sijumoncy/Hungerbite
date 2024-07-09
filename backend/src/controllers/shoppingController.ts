3afa776a13274e4b79e6ba8bdc2a35a4b7ca155d
sijumoncy <72241997+sijumoncy@users.noreply.github.com>
2024-07-09 21:59:47 +0530

    added restaurant endpoints to fetch data

--- backend/src/controllers/shoppingController.ts
+++ backend/src/controllers/shoppingController.ts
@@ -1,7 +1,11 @@
 import express, { NextFunction, Request, Response } from "express";
 import { VendorModel } from "../models";
 import { IFoodDoc } from "../models/food.model";
+import { findVendor } from "./adminController";
 
+/**
+ * get available foods based on pincode
+ */
 export const getFoodAvailability = async (
   req: Request,
   res: Response,
@@ -23,6 +27,9 @@ export const getFoodAvailability = async (
   return res.status(400).json({ message: "No food available" });
 };
 
+/**
+ * get top 10 restaurants in the pincode
+ */
 export const getTopRestaurants = async (
   req: Request,
   res: Response,
@@ -43,6 +50,10 @@ export const getTopRestaurants = async (
   return res.status(400).json({ message: "No restaurants found" });
 };
 
+/**
+ * get foods that can be ready in a given time
+ * time should be in minute
+ */
 export const getFoodInCertainTime = async (
   req: Request,
   res: Response,
@@ -71,14 +82,44 @@ export const getFoodInCertainTime = async (
   return res.status(400).json({ message: "No food available" });
 };
 
+/**
+ * search foods based on pincode
+ * TODO : later extend with keyword search
+ */
 export const searchFoods = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => {};
+) => {
+  const userPincode = req.params.pincode;
 
+  const result = await VendorModel.find({
+    pincode: userPincode,
+    serviceAvailable: true,
+  }).populate("foods");
+
+  if (result.length > 0) {
+    const foods = result.map((doc) => doc.foods);
+    return res.status(200).json(foods);
+  }
+
+  return res.status(400).json({ message: "No food available" });
+};
+
+/**
+ * get food and restaurant details by id
+ */
 export const getRestaurantDetails = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => {};
+) => {
+  const id = req.params.id;
+  const restaurant = await VendorModel.findById(id).populate("foods");
+
+  if (restaurant) {
+    return res.status(200).json(restaurant);
+  }
+
+  return res.status(400).json({ message: "Restaurant not found" });
+};
