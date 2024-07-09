f1485b606549aca3555c3ef0af0926310709b680
sijumoncy <72241997+sijumoncy@users.noreply.github.com>
2024-07-09 22:54:45 +0530

    user signup and validation, routes

---
+++ backend/src/controllers/userController.ts
@@ -0,0 +1,104 @@
+import express, { NextFunction, Request, Response } from "express";
+
+import { validate } from "class-validator";
+import { plainToClass } from "class-transformer";
+import { SignupUserInputs } from "../dto/user.dto";
+import { generateOneTimePassword, generateSalt, hashPassword } from "../utils";
+import { UserModel } from "../models/user.model";
+import { generateToken } from "../utils/token";
+
+/**
+ * signup user
+ */
+export const userSignup = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const userSignupInputs = plainToClass(SignupUserInputs, req.body);
+
+  const InputErrors = await validate(userSignupInputs, {
+    ValidationError: { target: true },
+  });
+
+  if (InputErrors.length > 0) {
+    return res.status(411).json(InputErrors);
+  }
+
+  const { email, password, phone } = userSignupInputs;
+
+  const salt = await generateSalt();
+  const hashedPwd = await hashPassword(password, salt);
+
+  const { otp, expiry: otp_expiry } = await generateOneTimePassword();
+
+  const result = await UserModel.create({
+    email: email,
+    password: hashedPwd,
+    salt: salt,
+    otp: otp,
+    phone: phone,
+    otp_expiry: otp_expiry,
+    firstName: "",
+    lastName: "",
+    address: "",
+    verified: false,
+    lat: 0,
+    lng: 0,
+  });
+
+  if (result) {
+    // send otp
+    // generate Token
+    const token = await generateToken({})
+    // response
+    return res
+      .status(201)
+      .json({ message: "registration successful", data: result, token : token  });
+  }
+};
+
+/**
+ * user login
+ */
+export const userLogin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
+
+/**
+ * verify user
+ */
+export const userVerify = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
+
+/**
+ * otp generate
+ */
+export const generateOTP = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
+
+/**
+ * get user profile
+ */
+export const getUserProfile = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
+
+/**
+ * update user profile
+ */
+export const updateUserProfile = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
---
+++ backend/src/dto/user.dto.ts
@@ -0,0 +1,12 @@
+import { IsEmail, IsEmpty, Length } from "class-validator";
+
+export class SignupUserInputs {
+  @IsEmail()
+  email: string;
+
+  @Length(10)
+  phone: string;
+
+  @Length(6, 12)
+  password: string;
+}
