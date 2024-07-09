d7b07409019515d4ac8aeb156d1084d8cdda5b36
sijumoncy <72241997+sijumoncy@users.noreply.github.com>
2024-07-09 17:55:39 +0530

    refactored app startup

---
+++ backend/src/app.ts
@@ -0,0 +1,20 @@
+import express, { Application } from "express";
+import bodyParser from "body-parser";
+
+import { adminRoute, vendorRoute } from "./routes";
+import path from "path";
+
+export default async (app: Application) => {
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  // image storage path config
+  app.use("/images", express.static(path.join(__dirname, "images")));
+
+  /**
+   * Config Routes
+   */
+  app.use("/admin", adminRoute);
+  app.use("/vendor", vendorRoute);
+
+  return app;
+};
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,31 +1,22 @@
 import express from "express";
-import bodyParser from "body-parser";
-
-import { adminRoute, vendorRoute } from "./routes";
+import App from "./app";
 import { connectDB } from "./config/db";
 import dotenv from "./config/dotenv";
-import path from "path";
 
 const PORT = dotenv.PORT;
 
-const app = express();
+const startServer = async () => {
+  const app = express();
+  /**
+   * Connect database
+   */
+  connectDB().then(async () => {
+    await App(app);
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-// image storage path config
-app.use("/images", express.static(path.join(__dirname, "images")));
-
-/**
- * Config Routes
- */
-app.use("/admin", adminRoute);
-app.use("/vendor", vendorRoute);
-
-/**
- * Connect database
- */
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
   });
-});
+};
+
+startServer();
