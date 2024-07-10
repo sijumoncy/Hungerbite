4937b19b799e39632f75d1ba0f29b16e2a3e80dc
sijumoncy <72241997+sijumoncy@users.noreply.github.com>
2024-07-10 10:19:03 +0530

    build setup and code cleanup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,11 @@
 import express, { Application } from "express";
-import bodyParser from "body-parser";
 
 import { adminRoute, vendorRoute, shoppingRoute, userRoute } from "./routes";
 import path from "path";
 
 export default async (app: Application) => {
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
   // image storage path config
   app.use("/images", express.static(path.join(__dirname, "images")));
 
